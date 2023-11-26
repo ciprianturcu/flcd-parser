58a1f21666efcb3fa5aaadd9522211fc814e5e2a
ciprianturcu <ciprianstelian_08@yahoo.com>
2023-11-26 15:44:51 +0200

    Class creation and todos for implementation

---
+++ Grammar.java
@@ -0,0 +1,6 @@
+public class Grammar {
+    //TODO - implementation of structure (fields {set of nonterminals, set of terminals, set of productions, productions for a given nonterminal}, constructor)
+    //TODO - read from file
+    //TODO - check CFG
+    //TODO - output (toString or custom printing function for the fields )
+}
