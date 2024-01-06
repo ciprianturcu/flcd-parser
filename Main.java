fa28c1d1b05ce994d55e17234638391951ca6ad6
AlexandraViorel <alexsig22@yahoo.com>
2024-01-06 18:42:02 +0200

    Solved the conflicts for the grammar g2 and added logs to the parseTable method.

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ public class Main {
 
     public static void cases() {
         Grammar g = new Grammar();
-        g.readFromFile("resources/g4.in");
+        g.readFromFile("resources/g2.in");
         LL1 ll1 = new LL1(g);
         ll1.FIRST();
         ll1.FOLLOW();
